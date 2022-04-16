faaead77e6b2e577c7a8daabb7b8e9054b359891
detroitcr <vikas98cr@gmail.com>
2022-04-16 20:44:25 +0530

    ss

---
+++ lib/intro/otp_screen.dart
@@ -0,0 +1,22 @@
+
+
+import 'package:flutter/material.dart';
+
+
+class OTPSCREEN extends StatelessWidget {
+  const OTPSCREEN({ Key? key }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+      body:Row(
+        children: [
+          Text('data'),
+          Text('data'),
+        ],
+      ) ,
+      
+    );
+  }
+}
